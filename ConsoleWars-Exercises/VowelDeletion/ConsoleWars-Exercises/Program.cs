118ec570f4f15a2930726cd03dfda4f194c4f478
Christian Scheel <christian.scheel@bluewin.ch>
2021-02-24 14:09:31 +0100

    Add ConsoleWars Exercises

---
+++ ConsoleWars-Exercises/VowelDeletion/ConsoleWars-Exercises/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleWars_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some text: ");
+            string userInputText = Console.ReadLine();
+            Console.WriteLine("Text without vowels (except y): " + Disemvowel(userInputText));
+        }
+
+        public static string Disemvowel(string userInputText)
+        {
+            string noVowelsUserInputText = userInputText;
+            string[] charsToRemove = { "A", "a", "E", "e", "I", "i", "O", "o", "U", "u" };
+
+            foreach (string a in charsToRemove)
+            {
+                noVowelsUserInputText = noVowelsUserInputText.Replace(a, string.Empty);
+            }
+            return noVowelsUserInputText;
+        }
+    }
+}
