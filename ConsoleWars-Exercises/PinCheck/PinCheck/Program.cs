44d117d09e467d2457c9951a5be1174f37364cde
Christian Scheel <christian.scheel@bluewin.ch>
2021-02-26 15:46:49 +0100

    Add New Project PinCheck

---
+++ ConsoleWars-Exercises/PinCheck/PinCheck/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PinCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your Pin: ");
+            string pin = Console.ReadLine();
+            Console.WriteLine(ValidatePin(pin));
+        }
+        public static bool ValidatePin(string pin)
+        {
+            //TODO: Refactor it. It works but not in a very smart way
+            char[] pinCharArray = pin.ToCharArray();
+            bool isDigit = false;
+
+            //checks if there are any non-digit values
+            foreach (char a in pinCharArray)
+            {
+                if (Char.IsDigit(a) == false)
+                {
+                    isDigit = false;
+                    break;
+                }
+                else
+                {
+                    isDigit = true;
+                }
+            }
+
+            if (isDigit)
+            {
+                if (pin.Length == 4 || pin.Length == 6)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
