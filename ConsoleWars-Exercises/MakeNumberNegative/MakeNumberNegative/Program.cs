29c63a88589ae36beebff07ef3a762f75ba0d096
Christian Scheel <christian.scheel@bluewin.ch>
2021-03-18 14:43:27 +0100

    Add project MakeNumberNegative

---
+++ ConsoleWars-Exercises/MakeNumberNegative/MakeNumberNegative/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MakeNumberNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert an int number");
+
+            string userInput = Console.ReadLine();
+            //no check if input is a text because not required by this exercise
+            int userNumber = Convert.ToInt32(userInput);
+
+            Console.WriteLine(MakeNegative(userNumber));
+        }
+
+        public static int MakeNegative(int number)
+        {
+            if (number >= 0)
+            {
+                return number * -1;
+            }
+            else
+            {
+                return number;
+            }
+        }
+    }
+}
