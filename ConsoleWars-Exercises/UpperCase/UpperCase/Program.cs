214f37f7efe332b9aa5179d09bbea07e5714600d
Christian Scheel <christian.scheel@bluewin.ch>
2021-03-18 14:25:30 +0100

    Add project UpperCase

---
+++ ConsoleWars-Exercises/UpperCase/UpperCase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert Jaden text: ");
+            string jadenText = Console.ReadLine();
+
+            Console.WriteLine(ToJadenCase(jadenText));
+        }
+
+        public static string ToJadenCase(string phrase)
+        {
+            return phrase.ToUpper();
+        }
+    }
+}
