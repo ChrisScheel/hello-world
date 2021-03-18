5e601785899494a198e4be6f95afbd1ae45abd1a
Christian Scheel <christian.scheel@bluewin.ch>
2021-03-18 15:17:34 +0100

    Add project Sum of the two lowest positive Integer in an Array

---
+++ ConsoleWars-Exercises/SumOf2LowestPosInt/SumOf2LowestPosInt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SumOf2LowestPosInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] posInt = { 19, 4, 42, 2, 77 };
+
+            if (posInt.Length >= 4)
+            {
+                Console.WriteLine(sumTwoSmallestNumbers(posInt));
+            }
+            else
+            {
+                Console.WriteLine("Please use an array which consists of at least 4 numbers");
+            }
+        }
+
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+                Array.Sort(numbers);
+ 
+                int lowestNumber = numbers[0];
+                int secondLowestNumber = numbers[1];
+                int sumOfLowestNumbers = lowestNumber + secondLowestNumber;
+
+                return sumOfLowestNumbers;
+        }
+    }
+}
