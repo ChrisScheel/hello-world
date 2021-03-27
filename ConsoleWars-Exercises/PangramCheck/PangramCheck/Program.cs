f168724162843d2d850e7d3d5652c52cc10c3c43
Christian Scheel <christian.scheel@bluewin.ch>
2021-03-27 16:31:40 +0100

    Add program to check if string is a pangram

---
+++ ConsoleWars-Exercises/PangramCheck/PangramCheck/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PangramCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a sentence to check if it is a pengram or not");
+
+            string userString = Console.ReadLine();
+            userString.ToLower();
+
+            Console.WriteLine(IsPangram(userString));
+        }
+
+        public static bool IsPangram(string str)
+        {
+            str = str.ToLower();
+            string[] alphabetCharacters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+            foreach (string alphabet in alphabetCharacters)
+            {
+                //if IndexOf() doesn't find the string it returns -1
+                if (str.IndexOf(alphabet) == -1)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
