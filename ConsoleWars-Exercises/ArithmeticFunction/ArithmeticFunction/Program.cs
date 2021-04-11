486b68ad64127081da7c1f3ef5f2e62722f3e5ca
Christian Scheel <christian.scheel@bluewin.ch>
2021-04-11 14:45:34 +0200

    Add new programm which calculates (add, substract, multiply, divide) two given numbers

---
+++ ConsoleWars-Exercises/ArithmeticFunction/ArithmeticFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArithmeticFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = 2;
+            double secondNumber = 4;
+
+            Console.WriteLine("Please insert \"add\", \"substract\", \"multiply\" or \"divide\"");
+            string op = Console.ReadLine();
+
+            Console.WriteLine(Arithmetic(firstNumber, secondNumber, op));
+        }
+
+        public static double Arithmetic(double a, double b, string op)
+        {
+            switch (op)
+            {
+                case "add":
+                    return a + b;
+                case "substract":
+                    return a - b;
+                case "multiply":
+                    return a * b;
+                case "divide":
+                    return a / b;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
