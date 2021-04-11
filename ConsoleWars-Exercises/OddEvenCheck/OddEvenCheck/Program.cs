74a74251dbe2850b5490dc6daf645b0559eee4d9
Christian Scheel <christian.scheel@bluewin.ch>
2021-04-11 14:11:59 +0200

    Add new programm which checks if the sum of an int array is odd or even

---
+++ ConsoleWars-Exercises/OddEvenCheck/OddEvenCheck/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OddEvenCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = {0, 1, 4, 1};
+
+            Console.WriteLine("Test if the sum of numbers is odd or even");
+            Console.WriteLine(OddOrEven(myArray));
+        }
+
+        public static string OddOrEven(int[] array)
+        {
+            int sum = 0;
+
+            foreach (int value in array)
+            {
+                sum += value;
+            }
+
+            if (sum%2 == 0 )
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }       
+        }
+    }
+}
