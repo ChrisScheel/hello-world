6dfc19686e6a0366d2d396fe1c32737342fbbb02
Christian Scheel <christian.scheel@bluewin.ch>
2021-04-15 16:04:33 +0200

    Add new program which filters out everything which is not an INT32

---
+++ ConsoleWars-Exercises/FilterStringsOutOfList/FilterStringsOutOfList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilterStringsOutOfList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> MyList = new List<object>();
+            MyList.Add("dd");
+            MyList.Add(2);
+            MyList.Add("a");
+            MyList.Add("b");
+            MyList.Add(12);
+            MyList.Add(4522);
+
+            foreach (int a in GetIntegersFromList(MyList))
+            {
+                Console.WriteLine(a);
+            }
+        }
+        //returns only Integer values. No doubles and so on
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            List<Int32> IntList = new List<Int32>();
+
+            foreach (var item in listOfItems)
+            {
+                Type t = item.GetType();
+
+                if (t.Equals(typeof(Int32)))
+                {
+                    IntList.Add(Convert.ToInt32(item));
+                }
+            }
+            return IntList;
+        }
+    }
+}
