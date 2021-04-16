5557c5f02f70bbb0ad6c067c942909695c7fbffd
Christian Scheel <christian.scheel@bluewin.ch>
2021-04-16 16:44:51 +0200

    Add new program which prints out the content of a list with the position of the string

---
+++ ConsoleWars-Exercises/PrintStringAndPosition/PrintStringAndPosition/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrintStringAndPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> MyList = new List<string>();
+            MyList.Add("a");
+            MyList.Add("b");
+            MyList.Add("c"); //TODO: if a string appears several times it will always print out the same position -> 3 in this case. do it again!
+            MyList.Add("c");
+            MyList.Add("c");
+
+            Console.WriteLine("The list contains the following words and their position: ");
+            foreach (string a in Number(MyList))
+            {
+                Console.WriteLine(a);
+            }
+        }
+
+        public static List<string> Number(List<string> lines)
+        {
+            List<string> OutPutList = new List<string>();
+
+            foreach(string a in lines)
+            {
+                //"+1+" was added to indexof() to print out position 1 and not 0, what could confuse the user
+                OutPutList.Add(Convert.ToString(lines.IndexOf(a) + 1) + ": " + a);
+            }
+            return OutPutList;
+        }
+    }
+}
