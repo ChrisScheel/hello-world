dc99df118c42838ddb793dff1f460ff24d9664e3
Christian Scheel <christian.scheel@bluewin.ch>
2021-04-18 16:28:26 +0200

    Add program which counts the number of correct smileys in an array

---
+++ ConsoleWars-Exercises/CountSmileys/CountSmileys/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CountSmileys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] smileyArray = {":)", ";)", ";}", ":-D"};
+
+            Console.WriteLine("The number of correct smileys is: " + CountSmileys(smileyArray));
+        }
+
+        public static int CountSmileys(string[] smileys)
+        {
+            int count = 0;
+
+            foreach (string smiley in smileys)
+            {
+                switch (smiley)
+                {
+                    case ":-)":
+                        count++;
+                        break;
+                    case ":-D":
+                        count++;
+                        break;
+                    case ":~)":
+                        count++;
+                        break;
+                    case ":~D":
+                        count++;
+                        break;
+                    case ":)":
+                        count++;
+                        break;
+                    case ":D":
+                        count++;
+                        break;
+                    case ";-)":
+                        count++;
+                        break;
+                    case ";-D":
+                        count++;
+                        break;
+                    case ";~)":
+                        count++;
+                        break;
+                    case ";~D":
+                        count++;
+                        break;
+                    case ";)":
+                        count++;
+                        break;
+                    case ";D":
+                        count++;
+                        break;
+                }
+            }
+            return count;
+        }
+    }
+}
