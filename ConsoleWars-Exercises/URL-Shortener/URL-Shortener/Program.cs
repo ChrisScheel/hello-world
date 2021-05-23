276fe60ba6692793e3b98dcd3a60f08d90cd7b54
Christian Scheel <christian.scheel@bluewin.ch>
2021-05-23 14:33:13 +0200

    Add application which deletes all chars after #

---
+++ ConsoleWars-Exercises/URL-Shortener/URL-Shortener/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace URL_Shortener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toBeShortenedURL = "www.codewars.com#about";
+            //string toBeShortenedURL = "www.codewars.com?page=1";
+
+            Console.WriteLine(RemoveUrlAnchor(toBeShortenedURL));
+        }
+
+        public static string RemoveUrlAnchor(string url)
+        {
+            string shortenedURL = String.Empty;
+            string toRemove = "#";
+
+            int i = url.IndexOf(toRemove);
+
+            if (i >= 0)
+            {
+                shortenedURL = url.Remove(i);
+                return shortenedURL;
+            }
+            return url;
+        }
+    }
+}
