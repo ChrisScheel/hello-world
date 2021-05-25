3b80f2f400e1f1bdda8995e71fae33922ea58320
Christian Scheel <christian.scheel@bluewin.ch>
2021-05-25 12:59:36 +0200

    Add application which sorts string array by length asc

---
+++ ConsoleWars-Exercises/SortStringArrayByLength/SortStringArrayByLength/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace SortStringArrayByLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] unsortedArray = {"Telescopes", "Glasses", "Eyes", "Monocles", "Hagass"};
+            string[] sortedArray = SortByLength(unsortedArray);
+
+            foreach (string s in sortedArray)
+            {
+                Console.WriteLine(s + " ");
+            }
+        }
+
+        /* Solution with Linq (thx Stackoverflow)
+         * TODO: Learn LINQ which is much easier for this type of exercise
+         */
+        public static string[] SortByLength(string[] array)
+        {
+            array = array.OrderBy(a => a.Length).ToArray();
+            return array;
+        }
+    }
+}
