ad3622e4215b6c03f97f70f0a6617d38737bca26
Christian Scheel <christian.scheel@bluewin.ch>
2021-05-26 10:32:31 +0200

    Add app which gives back the weight of 2 groups
    The application loops through an array with integers an gives back
    a new int array with the sum of weights of group A und B.

---
+++ ConsoleWars-Exercises/RowWeights/RowWeights/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace RowWeights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] peopleWeight = { 13, 27, 49 };
+            int[] peopleWeight = { 13 };
+
+            foreach (int n in RowWeights(peopleWeight))
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        public static int[] RowWeights(int[] a)
+        {
+            int totalGroupA = 0;
+            int totalGroupB = 0;
+
+            /*My beginner's mistake:
+             * "i <= a.Length" is 1 too much! With three variables for example 
+             * the length is 3 but the index only goes till 2
+             */
+            for (int i = 0; i < a.Length; i += 2)
+            {
+                totalGroupA += a[i];
+            }
+            for (int i = 1; i < a.Length; i += 2)
+            {
+                totalGroupB += a[i];
+            }
+
+            int[] weightGroupAB = { totalGroupA, totalGroupB };
+
+            return weightGroupAB;
+
+            /*Codewars solution because two for-loops might not be the best solution:
+             *  int[] result = new int[2];
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (i % 2 == 0)
+                        result[0] += a[i];
+                    else
+                        result[1] += a[i];
+                }
+               return result;
+            */
+        }
+    }
+}
