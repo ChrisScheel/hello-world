4f226d9e0c072b0500ccc8d34407442885853886
Christian Scheel <christian.scheel@bluewin.ch>
2021-06-01 15:24:29 +0200

    Add program which returns shortest Word in a string

---
+++ ConsoleWars-Exercises/ShortestWordInString/ShortestWordInString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace ShortestWordInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "turns out random test cases are easier than writing out basic ones";
+
+            Console.WriteLine(FindShort(input));
+        }
+
+        public static int FindShort(string s)
+        {
+            //The String.Split() method splits a string into an array of strings separated by the split delimeters.
+            string[] Words = s.Split(' ');
+
+            int intShortLength = Words[0].Length;
+
+            foreach (string word in Words)
+            {
+                if (word.Length < intShortLength)
+                {
+                    intShortLength = word.Length;
+                }
+            }
+
+            return intShortLength;
+        }
+    }
+}
