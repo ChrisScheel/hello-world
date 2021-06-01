216c96717a565dcf7873c66f74b782a6059cc7f4
Christian Scheel <christian.scheel@bluewin.ch>
2021-06-01 14:34:54 +0200

    Add program which prints int value based on binary

---
+++ ConsoleWars-Exercises/BinaryToInt/BinaryToInt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace BinaryToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] binaryNumbers = { 0, 1, 0, 1,};
+
+            Console.WriteLine("The number is {0}", binaryArrayToNumber(binaryNumbers));
+        }
+
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            StringBuilder binary = new StringBuilder();
+
+            foreach(int n in BinaryArray)
+            {
+                binary.Append(n);
+            }
+
+            string resultBinary = binary.ToString();
+            // The base of binary is 2. Therefore the the value of "Base" must be 2
+            return Convert.ToInt32(resultBinary, 2);
+        }
+    }
+}
