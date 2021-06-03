ae297e74e3d8821b5159d9a9049d2445c1605539
Christian Scheel <christian.scheel@bluewin.ch>
2021-06-03 15:29:16 +0200

    Add new app which greets the user

---
+++ ConsoleWars-Exercises/GreetMe/GreetMe/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace GreetMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please tell me your name ");
+            
+            string userName = Console.ReadLine();
+            Console.WriteLine(Greet(userName));
+        }
+
+        public static string Greet(string name)
+        {
+            //REFACTOR: Bad way with a lot of new strings. Use String Builder
+            string loweredName = name.ToLower();
+            char firstLetterOfName = loweredName[0];
+            char firstLetterOfNameUpper = char.ToUpper(firstLetterOfName);
+            string newName = firstLetterOfNameUpper + loweredName.Remove(0, 1);
+            
+            string greetUser = "Hello " + newName + "!";
+            return greetUser;
+            
+        }
+    }
+}
