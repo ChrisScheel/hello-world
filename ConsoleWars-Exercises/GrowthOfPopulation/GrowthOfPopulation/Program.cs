f0ea8eecb752bae0f6ee23305fba91f71f8615bd
Christian Scheel <christian.scheel@bluewin.ch>
2021-06-04 14:26:06 +0200

    Add program which calculates how long it take to reach goal population

---
+++ ConsoleWars-Exercises/GrowthOfPopulation/GrowthOfPopulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GrowthOfPopulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingPopulation = 1000;
+            double growthInPercentYearly = 0.02;
+            int additionalPeopleYearly = 50;
+            int goalPopulation = 1200;
+
+            Console.WriteLine("It takes {0} years until you reach your goal population", NbYear(startingPopulation, growthInPercentYearly, additionalPeopleYearly, goalPopulation));
+        }
+
+        public static int NbYear(int startingPopulation, double growthInPercentYearly, int additionalPeopleYearly, int goalPopulation)
+        {
+            int amountOfPeopleInYear = startingPopulation;
+            int years = 0;
+
+            while (amountOfPeopleInYear < goalPopulation)
+            {
+                amountOfPeopleInYear = CalculateAmountOfPeopleEndOfYear(amountOfPeopleInYear, growthInPercentYearly, additionalPeopleYearly);
+                years++;
+            }
+            return years;
+
+            static int CalculateAmountOfPeopleEndOfYear (int amountOfPeopleInYear, double growthInPercentYearly, int additionalPeopleYearly)
+            {
+                double NewAmountOfPeopleEndOfYear = (double)amountOfPeopleInYear + ((double)amountOfPeopleInYear * growthInPercentYearly) + additionalPeopleYearly;
+                return (int)NewAmountOfPeopleEndOfYear;
+            }
+        }
+    }
+}
