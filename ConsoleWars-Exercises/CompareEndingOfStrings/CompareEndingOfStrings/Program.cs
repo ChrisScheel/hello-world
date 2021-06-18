7bea1f0861772e981b3f98d2d76dce958d0c8b82
Christian Scheel <christian.scheel@bluewin.ch>
2021-06-18 14:47:35 +0200

    Add program which compares endings of two strings

---
+++ ConsoleWars-Exercises/CompareEndingOfStrings/CompareEndingOfStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CompareEndingOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = ":-)";
+            string word2 = "!@#$%^&*() :-)";
+
+            Console.WriteLine($"Does \"{word1}\" end with \"{word2}\"? " + Solution(word1, word2));
+        }
+
+        public static bool Solution(string str, string ending)
+        {
+            if (str.EndsWith(ending))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
